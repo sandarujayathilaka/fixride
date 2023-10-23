6dbd31e140b7f8c40e3eb708ce8258f388d4f204
IT21181856 <it21181856@my.sliit.lk>
2023-10-23 22:27:40 +0530

    wip

--- fixride/app/mac-list/[id].js
+++ fixride/app/mac-list/[id].js
@@ -1,12 +1,21 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import { router, useGlobalSearchParams } from "expo-router";
-import MechanicList from '../../src/components/MechanicList'
+import MechanicList from '../../src/components/MechanicList';
+import { useRoute } from "@react-navigation/native";
+import { useNavigation } from "@react-navigation/native";
+
 
 export default function AvailableMac() {
 
-   const param = useGlobalSearchParams();
-   const RequestId = param.Id;
+  //  const param = useGlobalSearchParams();
+  //  const RequestId = param.Id;
+
+   const route = useRoute();
+  
+   const { Requestid } = route.params;
+   let RequestId = Requestid;
+
   
 
   return (
